2a19a16732aafd79f6b87982f6b74ef1ec85b9a6
Guido <guido.pederzini@gmail.com>
2014-02-12 23:52:43 +0100

    altre slide su concurrency profiler

--- CDays14/CDays14.cpp
+++ CDays14/CDays14.cpp
@@ -9,38 +9,39 @@
 using namespace std;
 std::mutex g_pages_mutex;
 std::map<std::string, std::string> g_pages;
-std::map<std::string, std::string> g_pages2;
+
 
 void save_page(const std::string &url, int aTime)
 {
 	// simulate a long page fetch
-	std::this_thread::sleep_for(std::chrono::seconds(aTime));
+
 	std::string result = "fake content";
 
 	g_pages_mutex.lock(); // or, to be exception-safe, use std::lock_guard
+	std::this_thread::sleep_for(std::chrono::seconds(aTime));
 	g_pages[url] = result;	
 	g_pages_mutex.unlock();
 
-	g_pages_mutex.lock(); // or, to be exception-safe, use std::lock_guard
-	g_pages2[url] = result;
-	g_pages_mutex.unlock();
 	
 }
 
 int main()
 {
-
-	std::thread t1(save_page, "http://foo", 2);
-	std::thread t2(save_page, "http://bar", 7);
-	t1.join();
+	std::thread t2(save_page, "http://foo", 10);
+	std::thread t1(save_page, "http://bar", 17);
+	
 	t2.join();
+
+	t1.join();
+	
+	
+	
+
 
 	// safe to access g_pages without lock now, as the threads are joined
 	for (const auto &pair : g_pages) {
 		std::cout << pair.first << " => " << pair.second << '\n';
 	}
 
-	for (const auto &pair : g_pages2) {
-		std::cout << pair.first << " => " << pair.second << '\n';
-	}
+
 }
